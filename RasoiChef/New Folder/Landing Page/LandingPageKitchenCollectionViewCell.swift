00f7afa7254183a096d29ad3ed4d9359dac29d75
Shubham Jaiswal <shubhamjaiswal042003@gmail.com>
2025-01-23 00:11:16 +0530

    Updated the landing page Kitchens Ui

--- RasoiChef/New Folder/Landing Page/LandingPageKitchenCollectionViewCell.swift
+++ RasoiChef/New Folder/Landing Page/LandingPageKitchenCollectionViewCell.swift
@@ -12,11 +12,11 @@ class LandingPageKitchenCollectionViewCell: UICollectionViewCell {
     @IBOutlet var KichenImage: UIImageView!
     @IBOutlet var availabilityLabel: UILabel!
     @IBOutlet var KitchenName: UILabel!
-    @IBOutlet var ImageIcon: UIImageView!
+
     @IBOutlet var CuisineLabel: UILabel!
     @IBOutlet var Distance: UILabel!
     @IBOutlet var RatingsLabel: UILabel!
-    
+
     
     func updateLandingPageKitchen(for indexPath: IndexPath) {
         
@@ -26,7 +26,7 @@ class LandingPageKitchenCollectionViewCell: UICollectionViewCell {
         CuisineLabel.text = restaurant.cuisines.map { $0.rawValue }.joined(separator: ", ")
         RatingsLabel.text = "⭐ \(restaurant.rating)"
         KichenImage.image = UIImage(named: "KitchenImage 1")
-        ImageIcon.image = UIImage(named: "BreakfastIcon")
+        
         availabilityLabel.text = restaurant.isOnline ? "Online" : "Offline"
     }
 }
