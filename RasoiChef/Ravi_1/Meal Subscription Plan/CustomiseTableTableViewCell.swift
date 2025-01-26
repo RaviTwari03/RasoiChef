05a49b653da1b72b55a93623bbf8ad7ed04ee0c4
RaviTwari03 <ravirkt0@gmail.com>
2025-01-26 15:29:12 +0530

    meal subscription on the go

--- RasoiChef/Ravi_1/Meal Subscription Plan/CustomiseTableTableViewCell.swift
+++ RasoiChef/Ravi_1/Meal Subscription Plan/CustomiseTableTableViewCell.swift
@@ -9,13 +9,15 @@ import UIKit
 
 class CustomiseTableTableViewCell: UITableViewCell {
 
+  
     @IBOutlet var Breakfastbutton: UIButton!
     @IBOutlet var LunchButton: UIButton!
-    @IBOutlet var SnacksButton: UIButton!
-    @IBOutlet var DinnerButton: UIButton!
-   
     
-
+    @IBOutlet var SnacksButton: UIButton!
+    
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var DinnerButton: UIButton!
+    
 //    override func awakeFromNib() {
 //           super.awakeFromNib()
 //       }
@@ -57,40 +59,143 @@ class CustomiseTableTableViewCell: UITableViewCell {
 //           return newImage!
 //       }
 //   }
-    override func setSelected(_ selected: Bool, animated: Bool) {
-            super.setSelected(selected, animated: animated)
-        }
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//            super.setSelected(selected, animated: animated)
+//        }
+//
+//    func configure(dayMeals: [String]) {
+//           Breakfastbutton.setTitle(dayMeals[0], for: .normal)
+//           LunchButton.setTitle(dayMeals[1], for: .normal)
+//           SnacksButton.setTitle(dayMeals[2], for: .normal)
+//           DinnerButton.setTitle(dayMeals[3], for: .normal)
+//       }
+//
+//        private func setButtonImage(button: UIButton, iconName: String) {
+//            if let image = UIImage(named: iconName) {
+//                let resizedImage = resizeImage(image: image, targetSize: CGSize(width: 45, height: 45))
+//                button.setImage(resizedImage, for: .normal)
+//                button.imageView?.contentMode = .scaleAspectFit
+//            }
+//            button.setTitle(nil, for: .normal) // Remove default button title
+//        }
+//
+//        private func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+//            let size = image.size
+//            let widthRatio = targetSize.width / size.width
+//            let heightRatio = targetSize.height / size.height
+//            let newSize = CGSize(width: size.width * widthRatio, height: size.height * heightRatio)
+//
+//            UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+//            image.draw(in: CGRect(origin: .zero, size: newSize))
+//            let newImage = UIGraphicsGetImageFromCurrentImageContext()
+//            UIGraphicsEndImageContext()
+//
+//            return newImage!
+//    
+//
+//    }
+    // Callback to handle button taps
+//       var buttonAction: ((UIButton) -> Void)?
+//
+//       override func awakeFromNib() {
+//           super.awakeFromNib()
+//
+//           // Configure button appearance
+//           configureButton(Breakfastbutton)
+//           configureButton(LunchButton)
+//           configureButton(SnacksButton)
+//           configureButton(DinnerButton)
+//
+//           // Attach actions to buttons
+//           Breakfastbutton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+//           LunchButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+//           SnacksButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+//           DinnerButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+//       }
+//
+//       // Configure button style
+//       private func configureButton(_ button: UIButton) {
+//           button.layer.cornerRadius = 8
+//           button.layer.borderWidth = 1
+//           button.layer.borderColor = UIColor.lightGray.cgColor
+//           button.clipsToBounds = true
+//           button.setTitleColor(.darkGray, for: .normal)
+//       }
+//
+//       // Button tap handler
+//       @objc func buttonTapped(_ sender: UIButton) {
+//           buttonAction?(sender) // Trigger the callback when a button is tapped
+//       }
+//
+//       // Update cell with dynamic data
+//       func configure(day: String, meals: [String?]) {
+//           dayLabel.text = day
+//
+//           // Update button visibility and titles
+//           updateButton(Breakfastbutton, with: meals[0])
+//           updateButton(LunchButton, with: meals[1])
+//           updateButton(SnacksButton, with: meals[2])
+//           updateButton(DinnerButton, with: meals[3])
+//       }
+//
+//       private func updateButton(_ button: UIButton, with title: String?) {
+//           if let title = title {
+//               button.setTitle(title, for: .normal)
+//               button.isHidden = false
+//           } else {
+//               button.isHidden = true
+//           }
+//       }
+//   }
+    var buttonAction: ((UIButton) -> Void)?
 
-        // Configure cell with icons for the entire row
-        func configureRow(withIcons icons: [String]) {
-            // Ensure the icons array has 4 elements (Breakfast, Lunch, Snacks, Dinner)
-            guard icons.count == 4 else { return }
-            setButtonImage(button: Breakfastbutton, iconName: icons[0])
-            setButtonImage(button: LunchButton, iconName: icons[1])
-            setButtonImage(button: SnacksButton, iconName: icons[2])
-            setButtonImage(button: DinnerButton, iconName: icons[3])
-        }
+       override func awakeFromNib() {
+           super.awakeFromNib()
 
-        private func setButtonImage(button: UIButton, iconName: String) {
-            if let image = UIImage(named: iconName) {
-                let resizedImage = resizeImage(image: image, targetSize: CGSize(width: 45, height: 45))
-                button.setImage(resizedImage, for: .normal)
-                button.imageView?.contentMode = .scaleAspectFit
-            }
-            button.setTitle(nil, for: .normal) // Remove default button title
-        }
+           // Configure button appearance
+           configureButton(Breakfastbutton)
+           configureButton(LunchButton)
+           configureButton(SnacksButton)
+           configureButton(DinnerButton)
 
-        private func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
-            let size = image.size
-            let widthRatio = targetSize.width / size.width
-            let heightRatio = targetSize.height / size.height
-            let newSize = CGSize(width: size.width * widthRatio, height: size.height * heightRatio)
+           // Attach actions to buttons
+           Breakfastbutton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+           LunchButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+           SnacksButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+           DinnerButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+       }
 
-            UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-            image.draw(in: CGRect(origin: .zero, size: newSize))
-            let newImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
+       // Configure button style
+       private func configureButton(_ button: UIButton) {
+           button.layer.cornerRadius = 8
+           button.layer.borderWidth = 1
+           button.layer.borderColor = UIColor.lightGray.cgColor
+           button.clipsToBounds = true
+           button.setTitleColor(.darkGray, for: .normal)
+       }
 
-            return newImage!
-        }
-    }
+       // Button tap handler
+       @objc func buttonTapped(_ sender: UIButton) {
+           buttonAction?(sender) // Trigger the callback when a button is tapped
+       }
+
+       // Update cell with dynamic data
+       func configure(day: String, meals: [String?]) {
+           dayLabel.text = day
+
+           // Update button visibility and titles
+           updateButton(Breakfastbutton, with: meals[0])
+           updateButton(LunchButton, with: meals[1])
+           updateButton(SnacksButton, with: meals[2])
+           updateButton(DinnerButton, with: meals[3])
+       }
+
+       private func updateButton(_ button: UIButton, with title: String?) {
+           if let title = title {
+               button.setTitle(title, for: .normal)
+               button.isHidden = false
+           } else {
+               button.isHidden = true
+           }
+       }
+   }
--- RasoiChef/Ravi_1/Meal Subscription Plan/SubscriptionViewController.swift
+++ RasoiChef/Ravi_1/Meal Subscription Plan/SubscriptionViewController.swift
@@ -8,94 +8,186 @@
 import UIKit
 
 class SubscriptionViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
-
+    
     
     
     @IBOutlet var MealSubscriptionPlan: UITableView!
     
-    let mealIcons: [[String]] = [
-           ["BreakfastIcon", "LunchIcon", "SnacksIcon", "DinnerIcon"], // Monday
-           ["BreakfastIcon", "LunchIcon", "SnacksIcon", "DinnerIcon"], // Tuesday
-           ["BreakfastIcon", "LunchIcon", "SnacksIcon", "DinnerIcon"], // Wednesday
-           ["BreakfastIcon", "LunchIcon", "SnacksIcon", "DinnerIcon"], // Thursday
-           ["BreakfastIcon", "LunchIcon", "SnacksIcon", "DinnerIcon"], // Friday
-           ["BreakfastIcon", "LunchIcon", "SnacksIcon", "DinnerIcon"], // Saturday
-           ["BreakfastIcon", "LunchIcon", "SnacksIcon", "DinnerIcon"]  // Sunday
-       ]
-
-
+    struct DayMeal {
+        let day: String
+        var meals: [String?] // ["Breakfast", "Lunch", "Snacks", "Dinner"]
+    }
+    
+    var weeklyMeals: [DayMeal] = [
+        DayMeal(day: "Monday", meals: ["Breakfast", "Lunch", "Snacks", "Dinner"]),
+        DayMeal(day: "Tuesday", meals: ["Breakfast", "Lunch", "Snacks", "Dinner"]),
+        DayMeal(day: "Wednesday", meals: ["Breakfast", "Lunch", "Snacks", "Dinner"]),
+        DayMeal(day: "Thursday", meals: ["Breakfast", "Lunch", "Snacks", "Dinner"]),
+        DayMeal(day: "Friday", meals: ["Breakfast", "Lunch", "Snacks", "Dinner"]),
+        DayMeal(day: "Saturday", meals: ["Breakfast", "Lunch", "Snacks", "Dinner"]),
+        DayMeal(day: "Sunday", meals: ["Breakfast", "Lunch", "Snacks", "Dinner"])
+    ]
+    
+    
     override func viewDidLoad() {
-           super.viewDidLoad()
-           self.title = "Meal Subscription Plan"
-           // Register custom cells
-           MealSubscriptionPlan.register(UINib(nibName: "WeeklyPlans", bundle: nil), forCellReuseIdentifier: "WeeklyPlans")
-           MealSubscriptionPlan.register(UINib(nibName: "CustomiseTable", bundle: nil), forCellReuseIdentifier: "CustomiseTable")
-           
-           // Set the dataSource and delegate
-           MealSubscriptionPlan.dataSource = self
-           MealSubscriptionPlan.delegate = self
-       }
-
-       func numberOfSections(in tableView: UITableView) -> Int {
-           return 2 // Weekly Plans and Customize Table
-       }
-
-       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return 1
-       }
-
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-           if indexPath.section == 0 {
-              
-               let cell = tableView.dequeueReusableCell(withIdentifier: "WeeklyPlans", for: indexPath) as! WeeklyPlansTableViewCell
-              
-               return cell
-           } else {
-               guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomiseTable", for: indexPath) as? CustomiseTableTableViewCell else {
-                           return UITableViewCell()
-                       }
-
-                       // Configure the cell with icons for the respective day
-                       let iconsForDay = mealIcons[indexPath.row]
-                       cell.configureRow(withIcons: iconsForDay)
-
-                       return cell
-                   }
-           }
-       }
-
-       // Helper to return the day name based on row index
-       func getDayForIndex(index: Int) -> String {
-           let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-           return days[index]
-       }
-
-       // Section Footer for Payment Button
-       func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-           if section == 1 {
-               let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 60))
-               
-               // Add "Pay ₹1400" label
-               let payLabel = UILabel(frame: CGRect(x: 16, y: 10, width: 150, height: 40))
-               payLabel.text = "Pay ₹1400"
-               payLabel.font = UIFont.boldSystemFont(ofSize: 18)
-               footerView.addSubview(payLabel)
-               
-               // Add "Subscribe Plan" button
-               let button = UIButton(frame: CGRect(x: tableView.frame.width - 160, y: 10, width: 140, height: 40))
-               button.setTitle("Subscribe Plan", for: .normal)
-               button.backgroundColor = .orange
-               button.setTitleColor(.white, for: .normal)
-               button.layer.cornerRadius = 10
-               footerView.addSubview(button)
-               
-               return footerView
-           }
-           return nil
-       }
-
-       func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-           return section == 1 ? 60 : 0
-       }
-   
-
+        super.viewDidLoad()
+        self.title = "Meal Subscription Plan"
+        // Register custom cells
+        MealSubscriptionPlan.register(UINib(nibName: "WeeklyPlans", bundle: nil), forCellReuseIdentifier: "WeeklyPlans")
+        MealSubscriptionPlan.register(UINib(nibName: "CustomiseTable2", bundle: nil), forCellReuseIdentifier: "CustomiseTable2")
+        
+        // Set the dataSource and delegate
+        MealSubscriptionPlan.dataSource = self
+        MealSubscriptionPlan.delegate = self
+    }
+    
+    //       func numberOfSections(in tableView: UITableView) -> Int {
+    //           return 2 // Weekly Plans and Customize Table
+    //       }
+    //
+    //       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //           return 1
+    //       }
+    //
+    //       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //           if indexPath.section == 0 {
+    //
+    //               let cell = tableView.dequeueReusableCell(withIdentifier: "WeeklyPlans", for: indexPath) as! WeeklyPlansTableViewCell
+    //
+    //               return cell
+    //           } else {
+    //               guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomiseTable", for: indexPath) as? CustomiseTableTableViewCell else {
+    //                           return UITableViewCell()
+    //                       }
+    //
+    //                       // Configure the cell with icons for the respective day
+    //                       let iconsForDay = mealIcons[indexPath.row]
+    //                       cell.configureRow(withIcons: iconsForDay)
+    //
+    //                       return cell
+    //                   }
+    //           }
+    //       }
+    //
+    //       // Helper to return the day name based on row index
+    //       func getDayForIndex(index: Int) -> String {
+    //           let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    //           return days[index]
+    //       }
+    //
+    //       // Section Footer for Payment Button
+    //       func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+    //           if section == 1 {
+    //               let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 60))
+    //
+    //               // Add "Pay ₹1400" label
+    //               let payLabel = UILabel(frame: CGRect(x: 16, y: 10, width: 150, height: 40))
+    //               payLabel.text = "Pay ₹1400"
+    //               payLabel.font = UIFont.boldSystemFont(ofSize: 18)
+    //               footerView.addSubview(payLabel)
+    //
+    //               // Add "Subscribe Plan" button
+    //               let button = UIButton(frame: CGRect(x: tableView.frame.width - 160, y: 10, width: 140, height: 40))
+    //               button.setTitle("Subscribe Plan", for: .normal)
+    //               button.backgroundColor = .orange
+    //               button.setTitleColor(.white, for: .normal)
+    //               button.layer.cornerRadius = 10
+    //               footerView.addSubview(button)
+    //
+    //               return footerView
+    //           }
+    //           return nil
+    //       }
+    //
+    //       func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+    //           return section == 1 ? 60 : 0
+    //       }
+    //
+    //
+    //    override func viewDidLoad() {
+    //            super.viewDidLoad()
+    //        MealSubscriptionPlan.delegate = self
+    //        MealSubscriptionPlan.dataSource = self
+    //
+    //            // Register the custom cell
+    //            let nib = UINib(nibName: "CustomiseTable", bundle: nil)
+    //        MealSubscriptionPlan.register(nib, forCellReuseIdentifier: "CustomiseTable2")
+    //        }
+    //
+    //        func numberOfSections(in tableView: UITableView) -> Int {
+    //            return 2
+    //        }
+    //
+    //        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //            return 2
+    //        }
+    //
+    //        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //            guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomiseTable2", for: indexPath) as? CustomiseTableTableViewCell else {
+    //                fatalError("Unable to dequeue CustomiseCell")
+    //            }
+    //
+    //            let dayMeals = mealIcons[indexPath.section]
+    //            cell.configure(dayMeals: dayMeals)
+    //
+    //            return cell
+    //        }
+    //
+    //        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    //            return 60
+    //        }
+    //    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weeklyMeals.count
+    }
+    
+    //        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //            let cell = tableView.dequeueReusableCell(withIdentifier: "CustomiseTable2", for: indexPath) as! CustomiseTableTableViewCell
+    //
+    //            let dayMeal = weeklyMeals[indexPath.row]
+    //            cell.dayLabel.text = dayMeal.day
+    //
+    //            // Set button titles or images
+    //            cell.breakfastButton.setTitle(dayMeal.meals[0] ?? "", for: .normal)
+    //            cell.lunchButton.setTitle(dayMeal.meals[1] ?? "", for: .normal)
+    //            cell.snacksButton.setTitle(dayMeal.meals[2] ?? "", for: .normal)
+    //            cell.dinnerButton.setTitle(dayMeal.meals[3] ?? "", for: .normal)
+    //
+    //            // Add button actions
+    //            cell.breakfastButton.tag = indexPath.row * 4 + 0
+    //            cell.lunchButton.tag = indexPath.row * 4 + 1
+    //            cell.snacksButton.tag = indexPath.row * 4 + 2
+    //            cell.dinnerButton.tag = indexPath.row * 4 + 3
+    //            cell.breakfastButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    //            cell.lunchButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    //            cell.snacksButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    //            cell.dinnerButton.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    //
+    //            return cell
+    //        }
+    //    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomiseTable2", for: indexPath) as! CustomiseTableTableViewCell
+        
+        let dayMeal = weeklyMeals[indexPath.row]
+        cell.configure(day: dayMeal.day, meals: dayMeal.meals)
+        
+        // Handle button taps
+        cell.buttonAction = { [weak self] button in
+            guard let self = self else { return }
+            
+            let column = button == cell.Breakfastbutton ? 0 :
+            button == cell.LunchButton ? 1 :
+            button == cell.SnacksButton ? 2 : 3
+            
+            // Update model
+            self.weeklyMeals[indexPath.row].meals[column] = nil
+            
+            // Reload the specific row
+            self.MealSubscriptionPlan.reloadRows(at: [indexPath], with: .automatic)
+        }
+        
+        return cell
+    }
+}
