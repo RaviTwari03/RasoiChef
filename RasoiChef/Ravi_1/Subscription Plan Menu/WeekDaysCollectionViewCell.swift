733d8b5e90e002634308436589aff1658c4e7157
RaviTwari03 <ravirkt0@gmail.com>
2025-01-31 11:23:13 +0530

    updates

---
+++ RasoiChef/Ravi_1/Subscription Plan Menu/WeekDaysCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  WeekDaysCollectionViewCell.swift
+//  RasoiChef
+//
+//  Created by Ravi Tiwari on 31/01/25.
+//
+
+import UIKit
+
+class WeekDaysCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var weekDays: UILabel!
+    
+}
