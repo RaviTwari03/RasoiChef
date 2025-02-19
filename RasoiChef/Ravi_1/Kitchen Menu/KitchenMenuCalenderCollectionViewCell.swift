56060afc0324ba62994bf3fd2567ea507d40eb60
RaviTwari03 <ravirkt0@gmail.com>
2025-02-19 10:52:15 +0530

    commit

--- RasoiChef/Ravi_1/Kitchen Menu/KitchenMenuCalenderCollectionViewCell.swift
+++ RasoiChef/Ravi_1/Kitchen Menu/KitchenMenuCalenderCollectionViewCell.swift
@@ -73,6 +73,7 @@ class KitchenMenuCalenderCollectionViewCell: UICollectionViewCell {
             let red = CGFloat((rgb >> 16) & 0xFF) / 255.0
             let green = CGFloat((rgb >> 8) & 0xFF) / 255.0
             let blue = CGFloat(rgb & 0xFF) / 255.0
+             
             
             self.init(red: red, green: green, blue: blue, alpha: 1.0)
         }
